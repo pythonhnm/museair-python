b43623fbc6dbb7afcd58bfc711e7065d2cfd28be
pythonhnm <142024173+pythonhnm@users.noreply.github.com>
2024-10-19 12:02:10 +0800

    Update museair.py

--- museair.py
+++ museair.py
@@ -8,24 +8,23 @@ DEFAULT_SECRET =  [0x5ae31e589c56e17a,
 # ``AiryAi(0)`` calculated by Y-Cruncher. (48..56)
 INIT_RING_PREV = 0x33ea8f71bb6016d8
 seg = lambda s:s*8
-def rotate_left(num, k):  
-    bits = 64 
-    return (num << k) | (num >> (bits - k))
-
-def rotate_right(num, k):  
+def rotate_left(num, k):
     bits = 64
-    return (num >> k) | (num << (bits - k))
+    k %= bits
+    return ((num << k) | (num >> (bits - k)))&((1<<bits)-1)
+
+def rotate_right(num, k):
+    bits = 64
+    k %= bits
+    return ((num >> k) | (num << (bits - k)))&((1<<bits)-1)
 
 def wrapping_add(a,b,bits=64):
     mask = (1 << bits) - 1
     return (a + b) & mask
 
 def wrapping_sub(a, b, bits=64):  
-    mask = (1 << bits) - 1  
-    if b > a:  
-        return ((a + (1 << bits) - b) & mask)  
-    else: 
-        return (a - b) & mask
+    mask = (1 << bits) - 1
+    return (a - b) & mask
 
 def read_u32(b):
     return int.from_bytes(b[:4],'little')
@@ -37,12 +36,13 @@ def read_short(b):
     leng = len(b)
     if leng >= 4:
         off = (leng & 24) >> (leng >> 3)
-        return (read_u32(b) << 32) | read_u32(b[leng-4:leng]),(read_u32(b[off:leng]) << 32) | read_u32(b[leng-4-off:leng])
+        return ((read_u32(b) << 32)&0xFFFFFFFFFFFFFFFF) | read_u32(b[leng-4:]),((read_u32(b[off:]) << 32)&0xFFFFFFFFFFFFFFFF) | read_u32(b[leng-4-off:])
     elif leng > 0:
         # [0] [0] [0] @ len == 1 (0b01)
         # [0] [1] [1] @ len == 2 (0b10)
         # [0] [1] [2] @ len == 3 (0b11)
-        return (b[0]<<48)|(b[leng>>1]<<24)|b[leng-1],0
+        # MSB <-> LSB
+        return ((b[0]<<48)&0xFFFFFFFFFFFFFFFF)|((b[leng>>1]<<24)&0xFFFFFFFFFFFFFFFF)|b[leng-1],0
     else:
         return 0,0
 # convert from lib.rs
@@ -50,7 +50,7 @@ def u128_to_u64s(x):
     return x & 0xFFFFFFFFFFFFFFFF,(x >> 64) & 0xFFFFFFFFFFFFFFFF
 
 def u64s_to_u128(lo,hi):
-    return ((hi << 64) | lo) & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+    return (hi << 64) | (lo&0xFFFFFFFFFFFFFFFF)
 
 def wmul(a,b):
     return u128_to_u64s(a*b)
@@ -80,90 +80,89 @@ def _chixx(t,u,v):
 
 def _tower_layer_12(state,b,ring_prev,BFAST):
     if not BFAST:
-        state[0] ^= read_u64(b[seg(0):len(b)])
-        state[1] ^= read_u64(b[seg(1):len(b)])
+        state[0] ^= read_u64(b[seg(0):])
+        state[1] ^= read_u64(b[seg(1):])
         lo0,hi0 = wmul(state[0], state[1])
         state[0] = wrapping_add(state[0],ring_prev ^ hi0)
         
-        state[1] ^= read_u64(b[seg(2):len(b)])
-        state[2] ^= read_u64(b[seg(3):len(b)])
+        state[1] ^= read_u64(b[seg(2):])
+        state[2] ^= read_u64(b[seg(3):])
         lo1, hi1 = wmul(state[1], state[2])
         state[1] = wrapping_add(state[1],lo0 ^ hi1)
         
-        state[2] ^= read_u64(b[seg(4):len(b)])
-        state[3] ^= read_u64(b[seg(5):len(b)])
+        state[2] ^= read_u64(b[seg(4):])
+        state[3] ^= read_u64(b[seg(5):])
         lo2, hi2 = wmul(state[2], state[3])
         state[2] = wrapping_add(state[2],lo1 ^ hi2)
 
-        state[3] ^= read_u64(b[seg(6):len(b)])
-        state[4] ^= read_u64(b[seg(7):len(b)])
+        state[3] ^= read_u64(b[seg(6):])
+        state[4] ^= read_u64(b[seg(7):])
         lo3, hi3 = wmul(state[3], state[4])
         state[3] = wrapping_add(state[3],lo2 ^ hi3)
 
-        state[4] ^= read_u64(b[seg(8):len(b)])
-        state[5] ^= read_u64(b[seg(9):len(b)])
+        state[4] ^= read_u64(b[seg(8):])
+        state[5] ^= read_u64(b[seg(9):])
         lo4, hi4 = wmul(state[4], state[5])
         state[4] = wrapping_add(state[4],lo3 ^ hi4)
 
-        state[5] ^= read_u64(b[seg(10):len(b)])
-        state[0] ^= read_u64(b[seg(11):len(b)])
+        state[5] ^= read_u64(b[seg(10):])
+        state[0] ^= read_u64(b[seg(11):])
         lo5, hi5 = wmul(state[5], state[0])
         state[5] = wrapping_add(state[5],lo4 ^ hi5)
     else:
-        state[0] ^= read_u64(b[seg(0):len(b)])
-        state[1] ^= read_u64(b[seg(1):len(b)])
+        state[0] ^= read_u64(b[seg(0):])
+        state[1] ^= read_u64(b[seg(1):])
         lo0,hi0 = wmul(state[0], state[1])
         state[0] = ring_prev ^ hi0
         
-        state[1] ^= read_u64(b[seg(2):len(b)])
-        state[2] ^= read_u64(b[seg(3):len(b)])
+        state[1] ^= read_u64(b[seg(2):])
+        state[2] ^= read_u64(b[seg(3):])
         lo1, hi1 = wmul(state[1], state[2])
         state[1] = lo0 ^ hi1
         
-        state[2] ^= read_u64(b[seg(4):len(b)])
-        state[3] ^= read_u64(b[seg(5):len(b)])
+        state[2] ^= read_u64(b[seg(4):])
+        state[3] ^= read_u64(b[seg(5):])
         lo2, hi2 = wmul(state[2], state[3])
         state[2] = lo1 ^ hi2
 
-        state[3] ^= read_u64(b[seg(6):len(b)])
-        state[4] ^= read_u64(b[seg(7):len(b)])
+        state[3] ^= read_u64(b[seg(6):])
+        state[4] ^= read_u64(b[seg(7):])
         lo3, hi3 = wmul(state[3], state[4])
         state[3] = lo2 ^ hi3
 
-        state[4] ^= read_u64(b[seg(8):len(b)])
-        state[5] ^= read_u64(b[seg(9):len(b)])
+        state[4] ^= read_u64(b[seg(8):])
+        state[5] ^= read_u64(b[seg(9):])
         lo4, hi4 = wmul(state[4], state[5])
         state[4] = lo3 ^ hi4
 
-        state[5] ^= read_u64(b[seg(10):len(b)])
-        state[0] ^= read_u64(b[seg(11):len(b)])
+        state[5] ^= read_u64(b[seg(10):])
+        state[0] ^= read_u64(b[seg(11):])
         lo5, hi5 = wmul(state[5], state[0])
         state[5] = lo4 ^ hi5
     return state, lo5
 
 def _tower_layer_6(state,b,BFAST):
-    state[0],state[1] = _frac_6((state[0],state[1]),(read_u64(b[seg(0):len(b)]),read_u64(b[seg(1):len(b)])),BFAST)
-    state[2],state[3] = _frac_6((state[2],state[3]),(read_u64(b[seg(2):len(b)]),read_u64(b[seg(3):len(b)])),BFAST)
-    state[4],state[5] = _frac_6((state[4],state[5]),(read_u64(b[seg(4):len(b)]),read_u64(b[seg(5):len(b)])),BFAST)
+    state[0],state[1] = _frac_6((state[0],state[1]),(read_u64(b[seg(0):]),read_u64(b[seg(1):])),BFAST)
+    state[2],state[3] = _frac_6((state[2],state[3]),(read_u64(b[seg(2):]),read_u64(b[seg(3):])),BFAST)
+    state[4],state[5] = _frac_6((state[4],state[5]),(read_u64(b[seg(4):]),read_u64(b[seg(5):])),BFAST)
     return state
 
 def _tower_layer_3(state,b,BFAST):
-    state[0],state[3] = _frac_3((state[0],state[3]),read_u64(b[seg(0):len(b)]),BFAST)
-    state[1],state[4] = _frac_3((state[1],state[4]),read_u64(b[seg(1):len(b)]),BFAST)
-    state[2],state[5] = _frac_3((state[2],state[5]),read_u64(b[seg(2):len(b)]),BFAST)
+    state[0],state[3] = _frac_3((state[0],state[3]),read_u64(b[seg(0):]),BFAST)
+    state[1],state[4] = _frac_3((state[1],state[4]),read_u64(b[seg(1):]),BFAST)
+    state[2],state[5] = _frac_3((state[2],state[5]),read_u64(b[seg(2):]),BFAST)
     return state
 
 def _tower_layer_0(state,b,tot_len):
     i,j,k = 0,0,0
     leng = len(b)
-    assert leng <= seg(3)
     if leng <= seg(2):
-        (i, j) = read_short(b)
+        i, j = read_short(b)
         k = 0
     else:
-        i = read_u64(b[seg(0):leng])
-        j = read_u64(b[seg(1):leng])
-        k = read_u64(b[leng-seg(1):leng])
+        i = read_u64(b[seg(0):])
+        j = read_u64(b[seg(1):])
+        k = read_u64(b[leng-seg(1):])
     if tot_len >= seg(3):
         state[0],state[2],state[4] = _chixx(state[0], state[2], state[4])
         state[1],state[3],state[5] = _chixx(state[1], state[3], state[5])
@@ -196,10 +195,9 @@ def _tower_layer_x(a,tot_len,BFAST):
 
 def tower_loong(b,seed,BFAST):
     tot_len = len(b)
-    assert tot_len > 16
     off = 0
     rem = tot_len
-    state = DEFAULT_SECRET
+    state = DEFAULT_SECRET[:]
     state[0] = wrapping_add(state[0],seed)
     state[1] = wrapping_sub(state[1],seed)
     state[2] ^= seed
@@ -208,19 +206,21 @@ def tower_loong(b,seed,BFAST):
         state[4] = wrapping_sub(state[4],seed)
         state[5] ^= seed
         ring_prev = INIT_RING_PREV
-        while not rem < seg(12):
-            state,ring_prev = _tower_layer_12(state,b[off:tot_len],ring_prev,BFAST)
+        while True:
+            state,ring_prev = _tower_layer_12(state,b[off:],ring_prev,BFAST)
             off += seg(12)
             rem -= seg(12)
+            if rem < seg(12):
+                break
         state[0] ^= ring_prev
     if rem >= seg(6):
-        state = _tower_layer_6(state,b[off:tot_len],BFAST)
+        state = _tower_layer_6(state,b[off:],BFAST)
         off += seg(6)
         rem -= seg(6)
     if rem >= seg(3):
-        state = _tower_layer_3(state,b[off:tot_len],BFAST)
+        state = _tower_layer_3(state,b[off:],BFAST)
         off += seg(3)
-    return _tower_layer_x(_tower_layer_0(state,b[off:tot_len],tot_len),tot_len,BFAST)
+    return _tower_layer_x(_tower_layer_0(state,b[off:],tot_len),tot_len,BFAST)
 
 def tower_short(b,seed):
     leng = len(b)
@@ -287,8 +287,8 @@ def epi_loong_128(a,BFAST):
         lo0,hi0 = wmul(i, j)
         lo1,hi1 = wmul(j, k)
         lo2,hi2 = wmul(k, i)
-        i ^= lo0 ^ lo1 ^ hi2
-        j ^= hi0 ^ hi1 ^ lo2
+        i = lo0 ^ lo1 ^ hi2
+        j = hi0 ^ hi1 ^ lo2
     return u64s_to_u128(i, j)
 
 def base_hash(b,seed,BFAST):
@@ -299,9 +299,9 @@ def base_hash(b,seed,BFAST):
 
 def base_hash_128(b,seed,BFAST):
     if len(b) <= seg(2):
-        return int.from_bytes(epi_short_128(tower_short(b,seed),BFAST).to_bytes(16,'little'),'big')
+        return epi_short_128(tower_short(b,seed),BFAST)
     else:
-        return int.from_bytes(epi_loong_128(tower_loong(b,seed,BFAST),BFAST).to_bytes(16,'little'),'big')
+        return epi_loong_128(tower_loong(b,seed,BFAST),BFAST)
 
 def hash(b,seed):
     return base_hash(b,seed,False)
